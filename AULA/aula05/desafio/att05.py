3944ff3f5df41a911835b4b68b1c10f39b5fb2ca
RUBENSNETO0001 <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-11-26 15:57:33 -0500

    Create att05.py

---
+++ AULA/aula05/desafio/att05.py
@@ -0,0 +1,24 @@
+carrinhodeCompras = []
+
+while carrinhodeCompras != 0:
+    compras = input('Digite o nome do produto:')
+    carrinhodeCompras.append(compras)
+    
+remover = ''
+while remover != 'n'
+remover = input('Que remover algum:')
+if remover :
+    
+    
+numeros = []
+for i in range(5):
+    num = int(input('Digite numeros: '))
+    numeros.append(num)
+print(numeros)
+num02 = int(input('Me de um numero para remover:'))
+
+if  num02 in numeros  :
+    numeros.remove(num02) 
+    print(numeros)
+else:
+    print('Esse numero não ta aqui')
