7e37e6aa332a54d57d7938d07586328427ed8e8f
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-11-28 09:36:54 -0500

    fiz a estruruta do codigo e coloque o adiciona para funcionar

--- AULA/aula05/desafio/att7.py
+++ AULA/aula05/desafio/att7.py
@@ -1,7 +1,13 @@
-contato = {
-    'chave': 'nome',
+i_1 = 0 # e so um contador mesmo
+
+contato = []
+chave = {
+    'chave': 'nome'
+}
+numero = {
     'valor': 'numero'
 }
+
 menu = ''
 
 while menu != 0:
@@ -13,10 +19,49 @@ while menu != 0:
     menu = int(input('Digite aqui o numero da opção: '))
     
     if menu == 1:
-        print('Opção (Adicionar contato):')
-        nomeContato = input('Me informe o nome do contato: ')
-        numeroContato = input('Me informe o numero: ')
+        print('\n\n\nOpção (Adicionar contato):')
+        chave = input('Me informe o nome do contato: ')
+        numero = input('Me informe o numero: ')
         
-    contato = nomeContato
-    contato = numeroContato
-    print(contato)
+        contato.append(chave)
+        contato.append(numero)
+        
+        i_1 += 1
+        print('\n\nContatos:')
+        print('Quantidade de numeros(', i_1,'):')
+        for i in range(len(contato)):
+            print(contato[i])
+            if i == 1:
+                print('-------------------')
+    
+    # rever essa escolha, concertar
+    elif menu == 2:
+        print('\n\n\nOpção (Buscar contato):')
+        buscar = input('Digite o nome do contato: ')
+        
+        if contato == buscar:
+            local == len(contato)
+            
+            print('\n\nContato:')
+            print(chave[local])
+            print(numero[local])
+                
+            if i == 1:
+                print('-------------------')
+                        
+            else:
+                print('Esse contato não existe na lista')
+    #concertar aqui tbm
+    else:
+        print('\n\n\nOpção (Remover contato):')
+        remover = input('Digite o nome do contato: ')
+        
+        contato.Remover(remover)
+        
+        i_1 += 1
+        print('\n\nContatos:')
+        print('Quantidade de numeros(', i_1,'):')
+        for i in range(len(contato)):
+            print(contato[i])
+            if i == 1:
+                print('-------------------')
