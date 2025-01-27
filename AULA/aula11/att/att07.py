5d515c15acf1324d60520f44334d5bf93e15cc1e
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-01-27 11:01:03 -0300

    ssdad

--- AULA/aula11/att/att07.py
+++ AULA/aula11/att/att07.py
@@ -1,9 +1,9 @@
 class Livro:
     def __init__(self, matricula, nome, qtdPagina, datadelançamento):
+        self.
         
 class Usuario:
     def __init__(self):
         
 class Bibliotecario:
     def __init__(self):
-        asdas
