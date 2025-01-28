c96111729e2f91d5a50f0fda49cb4b402af13aa8
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-01-28 17:12:28 -0300

    pronto

--- AULA/aula12/aula12.py
+++ AULA/aula12/aula12.py
@@ -4,21 +4,23 @@ class Produto:
         self.quantidade = quantidade
         self.preco = preco
     def mostrarDAdos(self):
-        print('\n\n2[======================================]')
+        print('\n\n[======================================]')
         print(f'\tNome do produto: {self.nome}')
         print('[======================================]')
-        print(f'Qunatidade do produto: {self.quantidade}')
-        print(f'Preço do produto: {self.preco}')
+        print(f'\tQuantidade do produto: {self.quantidade}')
+        print(f'\tPreço do produto: {self.preco}')
         print('[======================================]')
 
 listaProduto = []
+print('[======================================]')
 qtd = int(input('Quantidade de produtos a ser colocado: '))
-
+print('[======================================]')
 for i in range(qtd):
+    print('\n[======================================]')
     variavel01 = input("Nome do produto: ").strip()
     variavel02 = int(input("Quantidade do produto: "))
     variavel03 = float(input("preco do produto: "))
-
+    print('\n[======================================]')
      # variavel para colocar no produto
     produto = Produto(variavel01, variavel02, variavel03)
     listaProduto.append(produto)
